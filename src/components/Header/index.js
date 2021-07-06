408ce8eb965206b4b49c7b03ed000bc641dfa6c5
Abner Willclefy <abnerwill.monteiro@gmail.com>
2021-07-06 10:58:24 -0300

    App finished

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,23 @@
 import Styles from './styles.module.css'
 import Logo from '../../assets/logo.svg'
-import Hero from '../../assets/desktop/image-hero.jpg'
+import HamIcon from '../../assets/icon-hamburger.svg'
+import CloseIcon from '../../assets/icon-close.svg'
+import { IconButton, makeStyles } from '@material-ui/core'
+import { useState } from 'react'
 
 export default function Header() {
+  const [display, setDisplay] = useState(false)
+
+  function handleDisplayChange() {
+    if(display) {
+      setDisplay(false)
+    } else {
+      setDisplay(true)
+    }
+  }
+
   return(
+    <>
       <div className={Styles.container}>
         <header>
           <img src={Logo} alt='Logotipo da empresa'/>
@@ -14,10 +28,25 @@ export default function Header() {
             <li>Products</li>
             <li>Support</li>
           </ul>
+          <IconButton onClick={handleDisplayChange}><img src={HamIcon} alt='' className={Styles.icon}/></IconButton>
         </header>
         <div className={Styles.description}>
           <h1>IMMERSIVE EXPERIENCES THAT DELIVER</h1>
         </div>
       </div>
+      <aside className={display ? Styles.displayed : Styles.noDisplayed}>
+        <header className={Styles.headerAside}>
+          <img src={Logo} alt='Logotipo da empresa'/>
+          <IconButton onClick={handleDisplayChange}><img src={CloseIcon} alt='' className={Styles.icon}/></IconButton>
+        </header>
+        <div className={Styles.titles}>
+          <h2>ABOUT</h2>
+          <h2>CAREERS</h2>
+          <h2>EVENTS</h2>
+          <h2>PRODUCTS</h2>
+          <h2>SUPPORT</h2>
+        </div>
+      </aside>
+    </>
   )
 }
